ec5eed1a3af8ff9a100879dd2602006a5e3c8d85
Hae <kanghae0817@gmail.com>
2024-12-10 13:00:02 +0100

    Añadir el archivo operacion.py

---
+++ operacion.py
@@ -0,0 +1,35 @@
+a = 4
+b = 2
+
+suma = a + b
+print(suma)
+
+resta = a - b
+print(resta)
+
+multiplicacion = a * b
+print(multiplicacion)
+
+division = a / b
+print(division)
+
+resto = a % b
+print(resto)
+
+exponente = a ** b
+print(exponente)
+
+cociente = a // b 
+print(cociente)
+
+#Probando exponente con números negativos
+exponente_a = -2 ** b
+print(exponente_a)
+exponente_b = (-2) ** b
+print(exponente_b)
+
+#Probando la pérdida de precisión
+c = 0.0000000001
+d = 1000000000
+resultado = d + c
+print(resultado)
