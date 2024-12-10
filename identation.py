3b2e18ef6f860dbb5a9e7fb9cbfa6a8f5fbde265
Hae <kanghae0817@gmail.com>
2024-12-10 11:21:47 +0100

    Añadir el archivo identation.py

---
+++ identation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+""" Probar identación en Python. """
+A = 1
+B = 3
+if A == B :
+    print("A es igual a B")
+else:
+    print("A y B no son iguales")
+
+print("Esto siempre se imprime")
+
